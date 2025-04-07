2b7728584f9128a87d09451e59189c7172dac694
migiwara5 <Mathieu.baudesson@gmail.com>
2025-04-07 14:20:11 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
+// CSS Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProjectApp from './ProjectApp';
-
-// CSS Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Rendu de l'application
 ReactDOM.createRoot(document.getElementById('root')).render(
