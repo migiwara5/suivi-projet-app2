9283be9452b8128c1d7646e9de2bda01da426786
migiwara5 <Mathieu.baudesson@gmail.com>
2025-04-08 10:23:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProjectApp from './ProjectApp';
+import LandingPage from './LandingPage';
 
-// Rendu de l'application
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ProjectApp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<ProjectApp />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
